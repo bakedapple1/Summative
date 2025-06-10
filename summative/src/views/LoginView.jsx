18e696cd4057c37c924ceedcd3406d7e52c83407
Daniel <bingrongcao@gmail.com>
2025-06-10 14:15:25 -0400

    Added default genres to OAuth users registering through LoginView

--- summative/src/views/LoginView.jsx
+++ summative/src/views/LoginView.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, fetchSignInMethodsForEmail } from 'firebase/auth'
-import { auth } from '../firebase';
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth, firestore } from '../firebase';
 import { useStoreContext } from "../context";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -10,41 +11,42 @@ import "./LoginView.css";
 function LoginView() {
     const navigate = useNavigate();
     const { setCurrentUser, selectedGenre } = useStoreContext();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [logEmail, setLogEmail] = useState("");
+    const [logPassword, setLogPassword] = useState("");
 
     async function checkLogin(event) {
         event.preventDefault();
 
         try {
-            const result = await signInWithEmailAndPassword(auth, email, password);
+            const result = await signInWithEmailAndPassword(auth, logEmail, logPassword);
             setCurrentUser(result.user);
             navigate(`/movies/genre/${selectedGenre}`);
         } catch (error) {
             if (error.code === 'auth/invalid-credential') {
                 alert("Email or password is incorrect.");
-                setPassword('');
+                setLogPassword('');
             } else {
                 alert("An error occurred during login. Please try again.");
-                setPassword('');
+                setLogPassword('');
             }
         }
     }
 
     const googleSignIn = async () => {
-        const provider = new GoogleAuthProvider();
-
         try {
+            const provider = new GoogleAuthProvider();
             const result = await signInWithPopup(auth, provider);
-            const methods = await fetchSignInMethodsForEmail(auth, result.user.email);
-
-            if (methods.length === 0) {
-                await result.user.delete();
-                alert("Please register first before signing in.");
+            const docRef = doc(firestore, "users", result.user.email);
+            const docSnap = await getDoc(docRef);
+            setCurrentUser(result.user);
+            if (!docSnap.exists()) {
+                const data = { preferredGenres: Array(5).fill(true).concat(Array(7).fill(false)) };
+                const docRef = doc(firestore, "users", result.user.email);
+                await setDoc(docRef, data);
             } else {
-                setCurrentUser(result.user);
-                navigate(`/movies/genre/${selectedGenre}`);
+                console.log("LoginView.jsx GoogleSignIn error");
             }
+            navigate(`/movies/genre/${selectedGenre}`);
         } catch (error) {
             console.log("Error signing in with Google:", error);
         }
@@ -58,9 +60,9 @@ function LoginView() {
                     <h1 className="login-label">Log In</h1>
                     <form className="login-form" id="login-form" onSubmit={(event) => { checkLogin(event) }}>
                         <label htmlFor="log-email" className="log-input-label">Email:</label>
-                        <input type="email" name="log-email" id="log-email" className="log-input" value={email} onChange={(event) => { setEmail(event.target.value) }} required />
+                        <input type="email" name="log-email" id="log-email" className="log-input" value={logEmail} onChange={(event) => { setLogEmail(event.target.value) }} required />
                         <label htmlFor="log-pass" className="log-input-label">Password:</label>
-                        <input type="password" name="log-pass" id="log-pass" className="log-input" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
+                        <input type="password" name="log-pass" id="log-pass" className="log-input" value={logPassword} onChange={(event) => { setLogPassword(event.target.value) }} required />
                         <input type="submit" form="login-form" value="Sign In" className="log-submit-button" id="log-submit" />
                     </form>
                     <button onClick={googleSignIn} className="google-signin">Sign In With Google</button>
